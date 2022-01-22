70383e372c0039017af9d84617b9f3735bdb9a2c
Valery <v.a.l.e.r.a95@mail.ru>
2022-01-23 00:31:16 +0300

    feat: correct sections and adaptive for mobile

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,25 @@
 console.log('\n Вёрстка соответствует макету. Ширина экрана 768px +48 \n 2) Ни на одном из разрешений до 320 px включительно не появляется горизонтальная полоса прокрутки + 15 \n 3) На ширине экрана 768 рх и меньше реализовано адаптивное меню + 22 ');
+
+const hamburgerButton = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+const navList = document.querySelector('.nav-list');
+const navLinks = document.querySelectorAll('.nav-link');
+const blackout = document.createElement('div');
+document.body.append(blackout);
+
+hamburgerButton.addEventListener('click', function() {
+    hamburgerButton.classList.toggle('is-active');
+    nav.classList.toggle('is-active');
+    navList.classList.toggle('is-active');
+    blackout.classList.toggle('blackout');
+});
+
+function closeMenu() {
+    if (event.target.classList.contains('nav-link')) {
+        hamburgerButton.classList.remove('is-active');
+        nav.classList.remove('is-active');
+        navList.classList.remove('is-active');
+        blackout.classList.toggle('blackout');
+    }
+}
+nav.addEventListener('click', closeMenu);
