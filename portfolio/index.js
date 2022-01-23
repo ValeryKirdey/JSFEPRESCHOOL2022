f6e2ef4fe1ddbba28e3568223eae5171ec1fe520
Valery <v.a.l.e.r.a95@mail.ru>
2022-01-23 11:54:29 +0300

    feat: finished edit adaptive version for mobile

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,25 +1,25 @@
 console.log('\n Вёрстка соответствует макету. Ширина экрана 768px +48 \n 2) Ни на одном из разрешений до 320 px включительно не появляется горизонтальная полоса прокрутки + 15 \n 3) На ширине экрана 768 рх и меньше реализовано адаптивное меню + 22 ');
 
-const hamburgerButton = document.querySelector('.hamburger');
+const hamburgerBtn = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
-const navList = document.querySelector('.nav-list');
-const navLinks = document.querySelectorAll('.nav-link');
-const blackout = document.createElement('div');
-document.body.append(blackout);
+const navUl = document.querySelector('.nav-ul');
+const navLinks = document.querySelectorAll('.nav-ul-a');
+const bgBlack = document.createElement('div');
+document.body.append(bgBlack);
 
-hamburgerButton.addEventListener('click', function() {
-    hamburgerButton.classList.toggle('is-active');
+hamburgerBtn.addEventListener('click', function() {
+    hamburgerBtn.classList.toggle('is-active');
     nav.classList.toggle('is-active');
-    navList.classList.toggle('is-active');
-    blackout.classList.toggle('blackout');
+    navUl.classList.toggle('is-active');
+    bgBlack.classList.toggle('bg-black');
 });
 
 function closeMenu() {
-    if (event.target.classList.contains('nav-link')) {
-        hamburgerButton.classList.remove('is-active');
+    if (event.target.classList.contains('.nav-ul-a')) {
+        hamburgerBtn.classList.remove('is-active');
         nav.classList.remove('is-active');
-        navList.classList.remove('is-active');
-        blackout.classList.toggle('blackout');
+        navUl.classList.remove('is-active');
+        bgBlack.classList.toggle('bg-black');
     }
 }
 nav.addEventListener('click', closeMenu);
