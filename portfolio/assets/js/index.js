56e6d042d78d9dd9b77eca975b9fa678651b8aea
Valery <v.a.l.e.r.a95@mail.ru>
2022-01-29 23:41:39 +0300

    feat: add styles for web page

--- portfolio/assets/js/index.js
+++ portfolio/assets/js/index.js
@@ -1,26 +1,26 @@
-const hamburgerbtn = document.querySelector('.hamburger');
-const nav = document.querySelector('nav');
-const navTitle = document.querySelector('.nav-title');
-const navReference = document.querySelectorAll('.nav-ref');
-const bgShadow = document.createElement('div');
-document.body.append(bgShadow);
+// const hamburgerbtn = document.querySelector('.hamburger');
+// const nav = document.querySelector('nav');
+// const navTitle = document.querySelector('.nav-title');
+// const navReference = document.querySelectorAll('.nav-ref');
+// const bgShadow = document.createElement('div');
+// document.body.append(bgShadow);
 
-hamburgerbtn.addEventListener('click', function() {
-    hamburgerbtn.classList.toggle('is-active');
-    nav.classList.toggle('is-active');
-    navTitle.classList.toggle('is-active');
-    bgShadow.classList.toggle('shadow');
-});
+// hamburgerbtn.addEventListener('click', function() {
+//     hamburgerbtn.classList.toggle('is-active');
+//     nav.classList.toggle('is-active');
+//     navTitle.classList.toggle('is-active');
+//     bgShadow.classList.toggle('shadow');
+// });
 
-function closeMenu() {
-    if (event.target.classList.contains('nav-ref')) {
-        hamburgerbtn.classList.remove('is-active');
-        nav.classList.remove('is-active');
-        navTitle.classList.remove('is-active');
-        bgShadow.classList.remove('shadow');
-    }
-}
-nav.addEventListener('click', closeMenu);
+// function closeMenu() {
+//     if (event.target.classList.contains('nav-ref')) {
+//         hamburgerbtn.classList.remove('is-active');
+//         nav.classList.remove('is-active');
+//         navTitle.classList.remove('is-active');
+//         bgShadow.classList.remove('shadow');
+//     }
+// }
+// nav.addEventListener('click', closeMenu);
 
 //----------------------------------------------------
 
