ff157817700511c72aa2883a565e88e2164dd613
Valery <v.a.l.e.r.a95@mail.ru>
2022-02-02 00:22:30 +0300

    feat: create first section

--- js-30/eco-sounds/index.js
+++ js-30/eco-sounds/index.js
@@ -1,1 +1,36 @@
-console.log('hey there')
+// const audio = document.querySelector('audio');
+
+// function playAudio() {
+//     audio.currentTime = 0;
+//     audio.play();
+// }
+
+// function pauseAudio() {
+//     audio.pause();
+// }
+
+// const audio = document.querySelector('audio');
+// const playBtn = document.querySelector('.play-btn');
+// const pauseBtn = document.querySelector('.pause-btn');
+
+// function playAudio() {
+//     audio.currentTime = 0;
+//     audio.play();
+// }
+
+// function pauseAudio() {
+//     audio.pause();
+// }
+
+// playBtn.addEventListener('click', playAudio);
+// pauseBtn.addEventListener('click', pauseAudio);
+
+//---------------------------------------------------
+
+document.body.addEventListener('click', e => {
+    if (!e.target.matches('button')) return
+    document.querySelector('.forest').src = e.target.dataset.item
+
+    document.querySelectorAll('button').forEach(btn => btn.classList.remove('active'))
+    e.target.classList.add('active')
+})
