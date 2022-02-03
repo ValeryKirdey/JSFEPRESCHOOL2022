2fab682ed1c8d6b08eed5fe05e47f43467733ca0
Valery <v.a.l.e.r.a95@mail.ru>
2022-02-03 17:30:19 +0300

    feat: fix header navigation

--- js-30/eco-sounds/index.js
+++ js-30/eco-sounds/index.js
@@ -9,28 +9,59 @@
 //     audio.pause();
 // }
 
-// const audio = document.querySelector('audio');
-// const playBtn = document.querySelector('.play-btn');
+const audio = document.querySelector('audio');
+const playBtn = document.querySelector('.play');
 // const pauseBtn = document.querySelector('.pause-btn');
 
-// function playAudio() {
-//     audio.currentTime = 0;
-//     audio.play();
-// }
+function playAudio() {
+    audio.currentTime = 0;
+    audio.play();
+}
 
 // function pauseAudio() {
 //     audio.pause();
 // }
 
-// playBtn.addEventListener('click', playAudio);
+playBtn.addEventListener('click', playAudio);
 // pauseBtn.addEventListener('click', pauseAudio);
 
 //---------------------------------------------------
+const navList = document.querySelector('.nav-list');
+const mainBackgraund = document.querySelectorAll('.main');
 
-document.body.addEventListener('click', e => {
-    if (!e.target.matches('button')) return
-    document.querySelector('.forest').src = e.target.dataset.item
+navList.addEventListener('click', updateMain);
 
-    document.querySelectorAll('button').forEach(btn => btn.classList.remove('active'))
-    e.target.classList.add('active')
-})
+function updateMain(event) {
+    if (event.target.classList.contains('nav-item')) {
+        const activeBtn = document.querySelector('.active');
+        const pressedBtn = event.target;
+        activeBtn.classList.remove('active');
+        pressedBtn.classList.add('active');
+        let item = event.target.dataset.item;
+        mainBackgraund.forEach((img) => img.src = `./assets/img/${item}.jpg`);
+    }
+}
+
+function preloadImages() {
+    const items = ['winter', 'thunder', 'summer', 'rain'];
+    items.forEach(elem => {
+        for (let i = 1; i <= 8; i++) {
+            const img = new Image();
+            img.src = `./assets/img/${elem}.jpg`;
+        }
+    });
+}
+
+preloadImages();
+
+//----------------------------------------------------
+
+
+
+// document.body.addEventListener('click', e => {
+//     if (!e.target.matches('button')) return
+//     document.querySelector('.forest').src = e.target.dataset.item
+
+//     document.querySelectorAll('button').forEach(btn => btn.classList.remove('active'))
+//     e.target.classList.add('active')
+// })
