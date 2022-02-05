d7ae4d282f8e7e633666b724a2e9a4f12c22a239
Valery <v.a.l.e.r.a95@mail.ru>
2022-02-05 20:55:13 +0300

    feat: fixed js file again

--- js-30/eco-sounds/index.js
+++ js-30/eco-sounds/index.js
@@ -1,167 +1,46 @@
-const navItems = document.querySelectorAll('.nav-item')
-const audio = document.querySelector('.audio')
-const playContainer = document.querySelector('.play-button-container')
-const logo = document.querySelector('.logo')
-let context;
-let src;
-let analyser;
-let init = false
-let currentItem = navItems[0]
-let showInfo = false;
+//---------------Change btn & sound----------------------
 
-
-setBackground('night')
-playContainer.addEventListener('click', playButtonHandler);
-//---------------------Play/Pause Audio-----------------------------------
-
-document.addEventListener('keydown', (e) => {
-    if (e.code === 'Space') playButtonHandler()
-    if (e.code === 'ArrowLeft' || e.code === 'ArrowRight') changeTabByKey(e.code)
-})
-
-logo.addEventListener('click', (e) => { setBackground('night') })
-
-for (let navItem of navItems) {
-    navItem.addEventListener('click', (e) => setActive(e.target.parentNode))
-}
-
-function changeTabByKey(key) {
-    let number = Number(currentItem.firstChild.dataset.order)
-    if (key == 'ArrowRight') number = (number < navItems.length - 1) ? number + 1 : 0
-    if (key == 'ArrowLeft') number = (number === 0) ? 7 : number - 1
-    setActive(navItems[number])
-}
-
-//---------------------Play/Pause Audio-----------------------------------
-
-
-const playPauseBtn = document.querySelector('.play');
-const playPauseLogo = document.querySelector('.logo');
-const playNavSounds = document.querySelector('[data-item]')
-
+const button = document.querySelector('button');
+const playBtn = document.querySelector('.play-btn');
+const pauseBtn = document.querySelector('pause');
+const audio = new Audio();
 let isPlay = false;
-const sound = new Audio();
 
 function playAudio() {
-    // audio.src = './assets/audio/night.mp3';
-    sound.src = sourceFinder();
-    sound.currentTime = 0;
     if (!isPlay) {
-        sound.play()
-        isPlay = true
+        audio.currentTime = 0;
+        audio.play();
+        isPlay = true;
     } else {
-        sound.pause();
-        isPlay = false
+        audio.pause();
+        isPlay = false;
     }
-    toggleButton()
-        // console.log(isPlay)
 }
+//---------------Change image & sound---------------------
 
-function sourceFinder() {
-    const soundItem = document.querySelectorAll('.nav-item')
-    soundItem.forEach((el) => {
-        if (el.classList.contains('active')) {
-            const soundName = el.dataset.bird
-            const sourse = `./assets/audio/${soundName}.mp3`
-            console.log(source)
+const navBtns = document.querySelectorAll('.nav-link');
+const navBtn = document.querySelector('.header-container');
+const videoImg = document.querySelectorAll('.img-video');
+
+function changeImage(event) {
+    const bird = event.target.dataset.bird;
+    if (event.target.classList.contains('nav-link')) {
+        videoImg.forEach((img) => img.src = `assets/img/${bird}.jpg`);
+        audio.src = `assets/audio/${bird}.mp3`;
+        navBtns.forEach((btn) => btn.classList.remove('active'));
+        event.target.classList.add('active');
+        if (isPlay == true) {
+            audio.play();
         }
-        return source
-    })
-
-}
-// function sourceFinder() {
-// let item = './assets/audio/night.mp3'
-// console.log(item)
-
-// for (let i = 1; i < 8; i++) {
-// if (soundItem[i].classList.contains('active')) {
-// let soundName = soundItem[i].dataset.item
-// item = `./assets/audio/${soundName}.mp3`
-// console.log(item)
-// return item
-// }
-// }
-// // for (el in birdItem) {
-// // if (birdItem[el].classList.contains('active')) {
-// // const soundName = birdItem[el].dataset.item
-// // source = `./assets/audio/${soundName}.mp3`
-// // console.log(source)
-// // return source
-// // }
-// // }
-// return item
-// }
-
-playPauseBtn.addEventListener('click', playAudio);
-playPauseLogo.addEventListener('click', playAudio);
-soundItem.addEventListener('click', playAudio)
-
-//---------------------Play/Pause Button-----------------------------------
-
-function playButtonHandler() {
-    isPlay = !isPlay
-    const target = document.querySelector('.play')
-    target.focus()
-    if (target.classList.contains('play-show') || target.classList.contains('play-hide')) {
-        target.classList.remove('play-show')
-        target.classList.remove('play-hide')
     }
-    animate(target, 'play-hide', true)
-        .then(() => {
-            playContainer.classList.remove(isPlay ? 'play' : 'pause')
-            animate(target, 'play-show', true)
-            playContainer.classList.add(isPlay ? 'pause' : 'play')
+}
+navBtn.addEventListener('click', changeImage);
 
-        })
-    playAudio()
+//-----------------Change btn---------------------------------
+
+function toggleBtn() {
+    button.classList.toggle('pause');
 }
 
-function animate(target, animation, remove) {
-    return new Promise(resolve => {
-        target.addEventListener('animationend', () => {
-            if (remove) target.classList.remove(animation)
-            resolve()
-        })
-        target.classList.add(animation)
-    })
-}
-//---------------------Background-----------------------------------
-
-function setActive(targetItem) {
-
-    if (targetItem.classList.contains('show') || targetItem.classList.contains('hide')) {
-        targetItem.classList.remove('show')
-        targetItem.classList.remove('hide')
-    }
-
-    const prevItem = document.querySelector('.nav-item.active')
-
-    const prevOrder = prevItem.firstChild.dataset.order
-    const currentOrder = targetItem.firstChild.dataset.order
-    const mainContainer = document.querySelector('.main')
-
-    let directionPrev = (prevOrder < currentOrder) ? 'align-end' : 'align-start'
-    let directionCurrent = (prevOrder < currentOrder) ? 'align-start' : 'align-end'
-
-    prevItem.classList.add(directionPrev)
-    animate(prevItem, 'deactive', true).then(() => prevItem.classList.remove(directionPrev))
-    navItems.forEach(e => e.classList.remove('active'))
-    animate(mainContainer, 'hide', true)
-        .then(() => setBackground(targetItem.firstChild.dataset.bird))
-        .then(() => animate(mainContainer, 'show', true))
-
-    targetItem.classList.add(directionCurrent)
-
-    animate(targetItem, 'active', false).then(() => targetItem.classList.remove(directionCurrent))
-    currentItem = targetItem
-    playAudio()
-}
-//---------------------Background-----------------------------------
-
-function setBackground(name) {
-    const mainContainer = document.querySelector('.main')
-    return new Promise(resolve => {
-        mainContainer.setAttribute('class', `main bg-${name}`)
-        resolve()
-    })
-}
+playBtn.addEventListener('click', toggleBtn);
+playBtn.addEventListener('click', playAudio);
