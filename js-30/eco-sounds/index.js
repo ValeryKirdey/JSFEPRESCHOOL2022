1df5afe3ddf8f839731bf790d1788ad1506fd8f3
Valery <v.a.l.e.r.a95@mail.ru>
2022-02-05 23:55:07 +0300

    feat: change play btn

--- js-30/eco-sounds/index.js
+++ js-30/eco-sounds/index.js
@@ -1,77 +1,56 @@
-//---------------Change btn & sound----------------------
-
-const button = document.querySelector('button');
-const playBtn = document.querySelector('.play-btn');
-const pauseBtn = document.querySelector('pause');
-const audio = new Audio();
+"use strict";
 let isPlay = false;
+let play_pause = document.querySelector(".play_pause");
+let play_pause2 = document.querySelector(".play_pause2");
+let link = document.querySelectorAll(".nav-link");
+let night = document.querySelector("#night");
+let forest = document.querySelector("#forest");
+let rain = document.querySelector("#rain");
+let summer = document.querySelector("#summer");
+let swamp = document.querySelector("#swamp");
+let thunder = document.querySelector("#thunder");
+let sea = document.querySelector("#sea");
+let winter = document.querySelector("#winter");
+let imgFon = document.querySelector(".container_img");
+forest.addEventListener("click", playAudio);
+night.addEventListener("click", playAudio);
+rain.addEventListener("click", playAudio);
+summer.addEventListener("click", playAudio);
+swamp.addEventListener("click", playAudio);
+thunder.addEventListener("click", playAudio);
+sea.addEventListener("click", playAudio);
+winter.addEventListener("click", playAudio);
+play_pause.addEventListener("click", playAudio2);
+play_pause2.addEventListener("click", playAudio2);
+const audio = new Audio();
+audio.src = `./assets/audio/night.mp3`;
 
-function playAudio() {
+function playAudio2() {
     if (!isPlay) {
-        audio.currentTime = 0;
         audio.play();
         isPlay = true;
+        play_pause.style.display = "none";
+        play_pause2.style.display = "block";
     } else {
         audio.pause();
         isPlay = false;
-    }
-    toggleButton()
-}
-
-
-// let isPlay = false;
-// const audio = new Audio();
-
-// function playAudio() {
-//     // audio.src = './assets/audio/night.mp3';
-//     audio.src = sourceFinder();
-//     audio.currentTime = 0;
-//     if (!isPlay) {
-//         audio.play()
-//         isPlay = true
-//     } else {
-//         audio.pause();
-//         isPlay = false
-//     }
-//     toggleButton()
-//         // console.log(isPlay)
-// }
-
-// function sourceFinder() {
-//     const soundItem = document.querySelectorAll('.nav-item')
-//     soundItem.forEach((el) => {
-//             if (el.classList.contains('active')) {
-//                 const soundName = el.dataset.bird
-//                 const sourse = `./assets/audio/${soundName}.mp3`
-//                     // console.log(source)
-//             }
-//             return source
-//         })
-//---------------Change image & sound---------------------
-
-const navBtns = document.querySelectorAll('.nav-link');
-const navBtn = document.querySelector('.header-container');
-const videoImg = document.querySelectorAll('.img-video');
-
-function changeImage(event) {
-    const bird = event.target.dataset.bird;
-    if (event.target.classList.contains('nav-link')) {
-        videoImg.forEach((img) => img.src = `assets/img/${bird}.jpg`);
-        audio.src = `assets/audio/${bird}.mp3`;
-        navBtns.forEach((btn) => btn.classList.remove('active'));
-        event.target.classList.add('active');
-        if (isPlay == true) {
-            audio.play();
-        }
+        play_pause.style.display = "block";
+        play_pause2.style.display = "none";
     }
 }
-navBtn.addEventListener('click', changeImage);
 
-//-----------------Change btn---------------------------------
+function playAudio(ev) {
+    for (let l of link) {
+        l.classList.remove("active");
+    }
+    let attribute = ev.target.getAttribute("id");
+    ev.target.classList.add("active");
+    audio.src = `./assets/audio/${attribute}.mp3`;
+    audio.currentTime = 0;
+    audio.play();
+    imgFon.style.backgroundImage = `url(./assets/img/${attribute}.jpg)`;
+    play_pause.style.display = "none";
+    play_pause2.style.display = "block";
+    isPlay = true;
 
-function toggleBtn() {
-    button.classList.toggle('pause');
 }
-
-playBtn.addEventListener('click', toggleBtn);
-playBtn.addEventListener('click', playAudio);
