32f2b008da079f8e3268a8cf8b5a8e057c110c91
Valery <v.a.l.e.r.a95@mail.ru>
2022-02-06 15:22:44 +0300

    feat: last edits

--- js-30/eco-sounds/index.js
+++ js-30/eco-sounds/index.js
@@ -1,103 +1,123 @@
-const navItems = document.querySelectorAll('.nav-item')
-const audio = document.querySelector('.audio')
-const playContainer = document.querySelector('.play')
-const logo = document.querySelector('.logo')
-let context;
-let src;
-let analyser;
-let init = false
-let isPlay = false
-let currentItem = navItems[0]
-let showInfo = false;
+const audio = new Audio();
+audio.src = './assets/audio/night.mp3'
+let isPlay = false;
+
+const btnPlay = document.querySelector('.button')
+const main = document.querySelector('.main-container')
+const btnLogo = document.querySelector('.logo')
+const btnNavLink = document.querySelectorAll('.nav-link')
+const btnForest = document.querySelector('.forest')
+const btnRain = document.querySelector('.rain')
+const btnSummer = document.querySelector('.summer')
+const btnSwamp = document.querySelector('.swamp')
+const btnThunder = document.querySelector('.thunder')
+const btnSea = document.querySelector('.sea')
+const btnWinter = document.querySelector('.winter')
 
 
-setBackground('forest')
-playContainer.addEventListener('click', playButtonHandler);
-//---------------------Play/Pause Audio-----------------------------------
 
-async function playAudio() {
-    if (!init) {
-        try {
-            let AudioContext = window.AudioContext || window.webkitAudioContext || false
-            if (AudioContext) {
-                context = new AudioContext();
-                src = context.createMediaElementSource(audio)
-                analyser = context.createAnalyser()
-                src.connect(analyser)
-                analyser.connect(context.destination);
-                analyser.fftSize = 64;
-                audio.addEventListener('play', visualizer);
-            }
-        } catch (e) {
-            console.log(e)
-        }
-        init = true
-    }
-    if (isPlay) {
-        audio.src = `./assets/audio/${currentItem.firstChild.dataset.item}.mp3`
-        await new Promise(resolve => audio.addEventListener('canplay', resolve))
+btnPlay.addEventListener('click', playSong)
+btnLogo.addEventListener('click', nightSong)
+btnForest.addEventListener('click', forestSong)
+btnRain.addEventListener('click', rainSong)
+btnSummer.addEventListener('click', summerSong)
+btnSwamp.addEventListener('click', swampSong)
+btnThunder.addEventListener('click', thunderSong)
+btnSea.addEventListener('click', seaSong)
+btnWinter.addEventListener('click', winterSong)
+
+
+
+function playSong() {
+    if (!isPlay) {
+
         audio.play()
+        btnPlay.classList.add('play')
+        isPlay = !isPlay
     } else {
         audio.pause()
+        audio.currentTime = 0
+        btnPlay.classList.remove('play')
+        isPlay = !isPlay
     }
 }
-//---------------------Play/Pause Button-----------------------------------
 
-function playButtonHandler() {
-    isPlay = !isPlay
-    const target = document.querySelector('.play') 
-    target.focus()  
-    if (target.classList.contains('play-show') || target.classList.contains('play-hide')) {
-        target.classList.remove('play-show')
-        target.classList.remove('play-hide')
-    }    
-    animate(target, 'play-hide', true)
-        .then(() => {
-            playContainer.classList.remove(isPlay ? 'play' : 'pause')
-            animate(target, 'play-show', true)
-            playContainer.classList.add(isPlay ? 'pause' : 'play')
-
-        })
-    playAudio()        
-}
-//---------------------Background-----------------------------------
-
-function setActive(targetItem) {
-    
-    if (targetItem.classList.contains('show') || targetItem.classList.contains('hide')) {
-        targetItem.classList.remove('show')
-        targetItem.classList.remove('hide')
-    }
-    
-    const prevItem = document.querySelector('.nav-item.active')
-        
-    const prevOrder = prevItem.firstChild.dataset.order
-    const currentOrder = targetItem.firstChild.dataset.order    
-    const mainContainer = document.querySelector('.main')
-
-    let directionPrev = (prevOrder < currentOrder) ? 'align-end' : 'align-start'
-    let directionCurrent = (prevOrder < currentOrder) ? 'align-start' : 'align-end'
-    
-    prevItem.classList.add(directionPrev)
-    animate(prevItem, 'deactive', true).then(() => prevItem.classList.remove(directionPrev))
-    navItems.forEach(e => e.classList.remove('active'))
-    animate(mainContainer, 'hide', true)
-        .then(() => setBackground(targetItem.firstChild.dataset.bird))
-        .then(() => animate(mainContainer, 'show', true))
-
-    targetItem.classList.add(directionCurrent)
-    
-    animate(targetItem, 'active', false).then(() => targetItem.classList.remove(directionCurrent))
-    currentItem = targetItem
-    playAudio()
-}
-//---------------------Background-----------------------------------
-
-function setBackground(name) {
-    const mainContainer = document.querySelector('.main')
-    return new Promise(resolve => {
-        mainContainer.setAttribute('class', `main bg-${name}`)
-        resolve()
-    })
+function stopSong() {
+    audio.pause()
+    btnPlay.classList.add('play')
+    isPlay = false
+    audio.currentTime = 0
 }
 
+
+function nightSong() {
+    stopSong()
+    audio.src = './assets/audio/night.mp3'
+    main.style.backgroundImage = "url('./assets/img/night.jpg')";
+    playSong()
+    btnNavLink.forEach(el => el.classList.remove('active'))
+    btnLogo.classList.add('active')
+}
+
+function forestSong() {
+    stopSong()
+    audio.src = './assets/audio/forest.mp3'
+    main.style.backgroundImage = "url('./assets/img/forest.jpg')";
+    playSong()
+    btnNavLink.forEach(el => el.classList.remove('active'))
+    btnForest.classList.add('active')
+}
+
+function rainSong() {
+    stopSong()
+    audio.src = './assets/audio/rain.mp3'
+    main.style.backgroundImage = "url('./assets/img/rain.jpg')";
+    playSong()
+    btnNavLink.forEach(el => el.classList.remove('active'))
+    btnRain.classList.add('active')
+}
+
+function summerSong() {
+    stopSong()
+    audio.src = './assets/audio/summer.mp3'
+    main.style.backgroundImage = "url('./assets/img/summer.jpg')";
+    playSong()
+    btnNavLink.forEach(el => el.classList.remove('active'))
+    btnSummer.classList.add('active')
+}
+
+function swampSong() {
+    stopSong()
+    audio.src = './assets/audio/swamp.mp3'
+    main.style.backgroundImage = "url('./assets/img/swamp.jpg')";
+    playSong()
+    btnNavLink.forEach(el => el.classList.remove('active'))
+    btnSwamp.classList.add('active')
+}
+
+function thunderSong() {
+    stopSong()
+    audio.src = './assets/audio/thunder.mp3'
+    main.style.backgroundImage = "url('./assets/img/thunder.jpg')";
+    playSong()
+    btnNavLink.forEach(el => el.classList.remove('active'))
+    btnThunder.classList.add('active')
+}
+
+function seaSong() {
+    stopSong()
+    audio.src = './assets/audio/sea.mp3'
+    main.style.backgroundImage = "url('./assets/img/sea.jpg')";
+    playSong()
+    btnNavLink.forEach(el => el.classList.remove('active'))
+    btnSea.classList.add('active')
+}
+
+function winterSong() {
+    stopSong()
+    audio.src = './assets/audio/winter.mp3'
+    main.style.backgroundImage = "url('./assets/img/winter.jpg')";
+    playSong()
+    btnNavLink.forEach(el => el.classList.remove('active'))
+    btnWinter.classList.add('active')
+}
