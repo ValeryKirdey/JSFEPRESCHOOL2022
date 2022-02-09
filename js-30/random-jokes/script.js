2d50d5a6390f74fac0854f046fa9d4955fe8c1ff
ValeryKirdey <v.a.l.e.r.a95@mail.ru>
2022-02-09 18:18:17 +0300

    feat: add animation for button

--- js-30/random-jokes/script.js
+++ js-30/random-jokes/script.js
@@ -12,15 +12,42 @@ function playSong() {
 btnPlay.addEventListener('click', playSong);
 
 
-function getdata() {
-    let datacur = document.querySelector('.quote');
-    fetch('https://api.chucknorris.io/jokes/random')
-        .then(response => response.json('rick-and-morty-quotes.json'))
-        .then(data => {
-            console.log(data)
-            datacur.innerHTML = data.value;
-        })
-        .catch(err => console.error())
+// function getdata() {
+//     let dataQuote = document.querySelector('.quote');
+//     // fetch('http://loremricksum.com/api/')
+//     //     .then(response => response.json())
+//     //     .then(data => {
+//     //         console.log(data)
+//     //         datacur.innerHTML = data.value;
+//     //     })
+//     //     .catch(err => console.error())
 
+//     fetch('http://loremricksum.com/api/')
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .then((data) => {
+//             console.log(data);
+//             dataQuote.innerHTML = data.value;
+//         });
+
+
+// }
+
+
+const urlQuote = 'rick-and-morty-quotes.json';
+
+async function getQuote() {
+    const resQuote = await fetch(urlQuote);
+    const quote = await resQuote.json();
+    showQuote(quote);
+}
+getQuote();
+
+function showQuote(quote) {
+    document.getElementsByName('.quote').src = './rick-and-morty-quotes.json';
 
 }
+
+const quoteBtn = document.querySelector('.btn');
+quoteBtn.addEventListener('click', getQuote);
