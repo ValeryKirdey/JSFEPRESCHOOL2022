76193cec0353f1c8d20139dd188b06fa67426cd4
ValeryKirdey <v.a.l.e.r.a95@mail.ru>
2022-02-09 15:09:46 +0300

    feat: add Rick sound for button

--- js-30/random-jokes/script.js
+++ js-30/random-jokes/script.js
@@ -1,1 +1,26 @@
-console.log('my jokes random app')
+const audio = new Audio();
+const btnPlay = document.querySelector('.btn');
+
+function playSong() {
+    audio.src = './assets/audio/Burp-A1-www.fesliyanstudios.com.mp3';
+    let isPlay = false
+    if (!isPlay) {
+        audio.play()
+    }
+}
+
+btnPlay.addEventListener('click', playSong);
+
+
+function getdata() {
+    let datacur = document.querySelector('.quote');
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => response.json('rick-and-morty-quotes.json'))
+        .then(data => {
+            console.log(data)
+            datacur.innerHTML = data.value;
+        })
+        .catch(err => console.error())
+
+
+}
