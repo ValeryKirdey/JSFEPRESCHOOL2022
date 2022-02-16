a6001426b8212f8e67f23d0291ad070bb67d3b30
ValeryKirdey <v.a.l.e.r.a95@mail.ru>
2022-02-16 13:27:37 +0300

    feat: add lang for r-j

--- js-30/random-jokes/rick-and-morty-quotes.js
+++ js-30/random-jokes/rick-and-morty-quotes.js
@@ -1,8 +1,8 @@
-let btn = document.getElementById('btn');
-btn.addEventListener('click', gen);
-let pun = document.getElementById('quote');
+// let btn = document.getElementById('btn');
+// btn.addEventListener('click', gen);
+// let pun = document.getElementById('quote');
 
-let dadJokes = [
+export const quotes1 = [
     "Ohh yea, you gotta get schwifty. -Rick",
     "He's not pressing charges… That's gotta be the \"you shot me\" equivalent of not being mad. -Rick",
     "Get off the high road Summer. We all got pink eye because you wouldn't stop texting on the toilet. -Rick",
@@ -131,11 +131,11 @@ let dadJokes = [
     "It's the hardest working liver in the Galaxy, Morty, and now it has a hole in it."
 ]
 
-function gen() {
-    const randomEl = Math.floor(Math.random() * dadJokes.length);
-    if (dadJokes[randomEl] === undefined) {
-        console.log('wait a second');
-    }
-    pun.innerText = `${dadJokes[randomEl]}`;
-    dadJokes.splice(randomEl, 1);
-}
+// function gen() {
+//     const randomEl = Math.floor(Math.random() * quotes.length);
+//     if (quotes[randomEl] === undefined) {
+//         console.log('wait a second');
+//     }
+//     pun.innerText = `${quotes[randomEl]}`;
+//     quotes.splice(randomEl, 1);
+// }
